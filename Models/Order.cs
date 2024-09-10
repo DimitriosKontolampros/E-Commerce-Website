fb1f19a6421f9a8aa62dccc26bd28072391a47aa
dimitrioskontolampros@gmail.com <dimitrioskontolampros@gmail.com>
2024-09-10 23:54:19 +0300

    Add project files.

---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string OrderDate { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public int TotalAmount { get; set; }
+        public string OrderStatus { get; set; } = string.Empty;
+    }
+
+}
+
+
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Price { get; set; } 
+        public int Stock { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+
+    }
+}
